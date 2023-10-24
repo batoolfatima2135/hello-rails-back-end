a7300099de73f5a0eaefeeca512a7313d24a6e2c
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-24 18:06:30 +0500

    Add greeting table with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello world")
+Greeting.create(message: "Hi")
+Greeting.create(message: "Hello, this is from rails backend")
+Greeting.create(message: "Hello, Have a good day")
+Greeting.create(message: "Hello to frontend")

4d51143f0b78a84ca2623dbd0d1e92f5541b9ab7
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-24 18:05:48 +0500

    Add greeting controller

---
+++ app/controllers/api/v1/greetings_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::GreetingsController < ApplicationController
+  def random_greeting
+    random_greeting = Greeting.order('RANDOM()').first
+    render json: { message: random_greeting.message }
+  end
+  
+end
